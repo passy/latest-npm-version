9e4a62e740914f5b7b7ec8d59d8e51d4d437e127
Pascal Hartig <passy@twitter.com>
2014-05-05 20:34:10 +0100

    Add command line arguments (close #5)

--- Main.hs
+++ Main.hs
@@ -1,16 +1,30 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
 
+import Data.Data (Data)
+import Data.Typeable (Typeable)
 import Npm.Latest (fetchLatestVersion)
-import System.Environment (getArgs)
+import System.Console.CmdArgs.Implicit (cmdArgsRun, (&=))
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified System.Console.CmdArgs.Implicit as CA
 
+
+data LatestNpmVersion = LatestNpmVersion {name :: String}
+    deriving (Show, Data, Typeable)
+
+args :: CA.Mode (CA.CmdArgs LatestNpmVersion)
+args = CA.cmdArgsMode $ LatestNpmVersion{name = CA.def &= CA.args}
+       &= CA.summary "latest-npm-version v0.1.0"
 
 main :: IO ()
 main = do
-    [name] <- getArgs
-    version <- fetchLatestVersion name
-    case version of
-        Nothing -> TIO.putStrLn "Error: fetching/parsing JSON failed"
-        Just v -> TIO.putStrLn $ T.unwords [T.pack name, v]
+    mainArgs <- cmdArgsRun args
+    let moduleName = (name mainArgs)
+    if length moduleName == 0
+        then TIO.putStrLn "Missing module name. Consult --help."
+        else do
+            version <- fetchLatestVersion moduleName
+            case version of
+                Nothing -> TIO.putStrLn "Error: fetching/parsing JSON failed"
+                Just v -> TIO.putStrLn $ T.unwords [T.pack moduleName, v]
