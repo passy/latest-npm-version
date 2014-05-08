a764c886b44590558139887398cfee6a7d42b652
Pascal Hartig <passy@twitter.com>
2014-05-08 23:49:25 +0100

    Add haddock (fix #6)

--- Npm/Latest.hs
+++ Npm/Latest.hs
@@ -1,5 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-|
+Module      : Npm.Latestj
+Description : Fetch the latest version of an npm module.
+Copyright   : (c) Pascal Hartig, 2014
+License     : MIT
+Maintainer  : phartig@rdrei.net
+Stability   : experimental
+Portability : POSIX
 
+This is just an experiment of me porting a node module to Haskell and learning
+about Lenses and Pipes along the way. Use on your own risk.
+-}
 module Npm.Latest (
     fetchLatestVersion,
     extractVersion
@@ -12,6 +23,7 @@ import qualified Data.Text as T
 latestUrl :: Format
 latestUrl = "https://registry.npmjs.org/{}/latest"
 
+-- |Fetch the latest version for the given module name.
 fetchLatestVersion :: String -> IO (Maybe T.Text)
 fetchLatestVersion name = do
     req <- buildRequest name latestUrl
