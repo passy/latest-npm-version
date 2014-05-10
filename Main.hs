f5ea03c155a303571f73fdf11635221b2e37106a
Pascal Hartig <passy@twitter.com>
2014-05-11 00:07:08 +0100

    Oh, right, versions

--- Main.hs
+++ Main.hs
@@ -16,7 +16,7 @@ data LatestNpmVersion = LatestNpmVersion {name :: String}
 
 args :: CA.Mode (CA.CmdArgs LatestNpmVersion)
 args = CA.cmdArgsMode $ LatestNpmVersion{name = CA.def &= CA.args}
-       &= CA.summary "latest-npm-version v0.1.0"
+       &= CA.summary "latest-npm-version v0.2.1"
 
 main :: IO ()
 main = do
