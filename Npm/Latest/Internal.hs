e775904e68210fa8790878fef31b6baa2e78f740
Pascal Hartig <passy@twitter.com>
2014-05-11 22:28:11 +0100

    Do you even liftM?

--- Npm/Latest/Internal.hs
+++ Npm/Latest/Internal.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DataKinds #-}
 module Npm.Latest.Internal (
     extractVersion,
     buildRequest,
@@ -6,13 +6,18 @@ module Npm.Latest.Internal (
 ) where
 
 import Control.Lens ((^?), _Right)
+import Control.Exception (catchJust)
+import Control.Monad (guard)
 import Control.Monad.Trans.State.Strict (evalStateT)
+import Control.Monad.IO.Class (liftIO)
+import Data.Maybe (fromMaybe)
 import Data.Aeson (json', Value)
 import Data.Aeson.Lens (key, _String, AsValue)
 import Data.Text.Format (Format, format)
 import Network.URI (escapeURIString, isUnreserved)
+import Network.HTTP.Client (HttpException(StatusCodeException))
 import Pipes.Attoparsec (parse, ParsingError)
-import Pipes.HTTP (parseUrl, withManager, tlsManagerSettings, withHTTP, responseBody, Request)
+import Pipes.HTTP (parseUrl, withManager, tlsManagerSettings, withHTTP, responseBody, Request, Manager)
 
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
@@ -25,8 +30,24 @@ buildRequest :: String -> Format -> IO Request
 buildRequest name urlFormat =
     parseUrl $ TL.unpack $ format urlFormat [escapeURIString isUnreserved name]
 
-makeVersionRequest :: Request -> IO (Maybe (Either ParsingError Value))
-makeVersionRequest req =
-    withManager tlsManagerSettings $ \mngr ->
-        withHTTP req mngr $ \resp ->
-            evalStateT (parse json') (responseBody resp)
+makeVersionRequest :: Request -> IO (Either ParsingError Value)
+makeVersionRequest req = undefined
+    -- withManager tlsManagerSettings $ \mngr -> executeHTTPRequest req mngr
+
+executeHTTPRequest :: Request -> Manager -> IO (Either ParsingError Value)
+executeHTTPRequest req mngr = do
+     -- TODO: simplify
+     catchJust (guard . isStatusCodeException)
+               (withHTTP req mngr $ \resp -> parseResponse resp >>=
+                return . unwrapMaybe)
+               (\ex -> undefined)
+               -- (\ex -> return $ Left (ex :: HttpException))
+     where
+        unwrapMaybe :: Maybe (Either a Value) -> Either a Value
+        unwrapMaybe v = fromMaybe (Right "nothing") v
+
+        parseResponse resp = evalStateT (parse json') (responseBody resp)
+
+isStatusCodeException :: HttpException -> Bool
+isStatusCodeException (StatusCodeException _ _ _) = True
+isStatusCodeException _ = False
