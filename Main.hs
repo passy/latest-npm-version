001861d0fa66a24676829f4a376362f3a7ee6bef
Pascal Hartig <passy@twitter.com>
2014-05-17 20:37:15 +0100

    Proper error handling for 404s, JSON parsing
    Fix #10
    Fix #9

--- Main.hs
+++ Main.hs
@@ -1,19 +1,26 @@
-{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable, ExistentialQuantification #-}
 
 import Paths_latest_npm_version (version)
-import Control.Exception (fromException)
-import Network.HTTP.Client (HttpException(StatusCodeException))
+import Control.Exception (fromException, Exception, SomeException)
+import Network.HTTP.Client (HttpException(StatusCodeException, HandshakeFailed))
+import Network.HTTP.Types.Status (statusCode)
 import Data.Data (Data)
 import Data.Version (showVersion)
 import Data.Typeable (Typeable)
 import Npm.Latest (fetchLatestVersion, GenericNpmException(..))
 import System.Console.CmdArgs.Implicit (cmdArgsRun, (&=))
 import System.Console.CmdArgs.Explicit (HelpFormat(..), helpText)
+import System.IO (stderr)
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified System.Console.CmdArgs.Implicit as CA
 
+
+data ErrHandler a = forall e . Exception e => ErrHandler (e -> a)
+
+matchErr :: SomeException -> b -> [ErrHandler b] -> b
+matchErr e = foldr (\(ErrHandler f) r -> maybe r f $ fromException e)
 
 data LatestNpmVersion = LatestNpmVersion {name :: String}
     deriving (Show, Data, Typeable)
@@ -32,16 +39,16 @@ main = do
     let moduleName = name mainArgs
     if null moduleName
         then print $ helpText [] HelpFormatDefault args
-        else
-            fetchLatestVersion moduleName >>=
-                TIO.putStrLn . either
-                    formatError
-                    id
+        else fetchLatestVersion moduleName >>= either handleErr TIO.putStrLn
 
-    where
-        formatError e =
-            T.pack $ "FATAL: " ++ case fromException e of
-                Just GenericNpmException -> "Unknown error"
-                _ -> case fromException e of
-                    Just (s@StatusCodeException{}) -> "HTTP error " ++ show s
-                    _ -> "Unknown error"
+handleErr :: SomeException -> IO ()
+handleErr err = TIO.hPutStrLn stderr $ matchErr err handleOther
+    [ErrHandler handleA, ErrHandler handleB] where
+        handleA GenericNpmException = "Invalid JSON data received."
+        handleB (StatusCodeException status _ _) =
+            if statusCode status == 404
+                then "Package not found."
+                else T.unwords
+                    ["Invalid status code ", T.pack $ show $ statusCode status]
+        handleB s@_ = T.unwords ["HTTP error: ", T.pack $ show s]
+        handleOther = "Unknown error."
