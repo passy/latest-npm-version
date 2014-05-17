4d9abc92467cb4f0b58d68b65adc248e3df1eeb0
Pascal Hartig <passy@twitter.com>
2014-05-17 23:18:24 +0100

    Fix tests (fix #11)

--- Tests/Main.hs
+++ Tests/Main.hs
@@ -2,21 +2,28 @@
 module Main where
 
 import Test.Hspec
-import Npm.Latest (extractVersion)
+import Control.Exception (SomeException(..), toException)
+import Npm.Latest (extractVersion, GenericNpmException(..))
 
 import qualified Data.Text as T
+
+instance Eq SomeException where
+    -- Terrible hack, but this is for tests, so it's fine, right?
+    a == b = show a == show b
 
 main :: IO ()
 main = hspec $ do
     describe "extractVersion" $ do
-        it "returns Just for a valid JSON object" $ do
+        it "is Right for a valid JSON object" $ do
             let json = T.unpack "{\"version\": \"1.0\"}"
-            extractVersion (Just $ Right $ json) `shouldBe` Just "1.0"
+            extractVersion (Right json) `shouldBe` (Right "1.0")
 
-        it "returns Nothing for a malformed JSON object" $ do
+        it "is Left for a malformed JSON object" $ do
             let json = T.unpack "{\"description\": \"not what we expect\"}"
-            extractVersion (Just $ Right $ json) `shouldBe` Nothing
+            extractVersion (Right $ json) `shouldBe`
+                (Left $ toException GenericNpmException)
 
-        it "returns Nothing for a malformed JSON object" $ do
+        it "is Left for a malformed JSON object" $ do
             let json = T.unpack "not even $ JSON"
-            extractVersion (Just $ Right $ json) `shouldBe` Nothing
+            extractVersion (Right $ json) `shouldBe`
+                (Left $ toException GenericNpmException)
